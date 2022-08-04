a235de37c0470c517020d2f59ddbe416d4a47d6d
Jonah <jonahcole@yahoo.com>
2022-08-03 17:02:08 -0700

    Add hyperlink url to each game object

--- games/serializers.py
+++ games/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
 from .models import Game
 
-class GameSerializer(serializers.ModelSerializer):
+class GameSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Game
-        fields = ('id', 'title', 'genre', 'description', 'release_date', 'developer', 'publisher', 'price')
+        fields = ('id', 'url', 'title', 'genre', 'description', 'release_date', 'developer', 'publisher', 'price')
         
